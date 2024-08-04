552fa08184f506837d970648e272d1f7eb3545fa
Muhiga <muhigadylan@gmail.com>
2024-08-04 13:15:56 +0300

    Added new files to Python ML

---
+++ Linear_Regression/eda_plots.py
@@ -0,0 +1,52 @@
+import seaborn as sns
+import pandas as pd
+import matplotlib.pyplot as plt
+import os
+
+data=pd.read_csv('cleaned_data.csv')
+
+def distribution_plots(df,output_dir, hue=None):
+# Function that takes 2 params:
+#1. df: the input dataframe
+#2. output_dir: where the data will be saved
+
+    
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    for column in data.columns:
+        plt.figure(figsize=(12,8))
+        if pd.api.types.is_numeric_dtype(df[column]):
+            sns.histplot(data[column], kde=True)
+            plt.title(f'Distribution Plot for {column}')
+            plt.xlabel(column)
+            plt.ylabel('Frequency')
+            plt.savefig(os.path.join(output_dir, f'{column}_distplot.png'))
+            plt.close()
+
+            plt.figure(figsize=(12,8))
+            sns.boxplot(data=df, x=column, showfliers=True)
+            plt.title(f'Box Plot for {column}')
+            plt.xlabel(column)
+            plt.savefig(os.path.join(output_dir, f'{column}_boxplot.png'))
+            plt.close()
+    if 'Unnamed: 0' in df.columns:
+        df=df.drop('Unnamed: 0', axis=1)
+    plt.figure(figsize=(10, 8))
+    sns.heatmap(df.corr(), annot=True, fmt=".2f", cmap="coolwarm")
+    plt.title('Correlation Heatmap')
+    plt.savefig(os.path.join(output_dir, 'heatmap.png'))
+    plt.close()
+
+    plt.figure()
+    sns.pairplot(df, hue=hue)
+    plt.title('Pair Plot')
+    plt.savefig(os.path.join(output_dir, 'pairplot.png'))
+    plt.close()
+    
+    
+    
+output_directory = 'eda_plots'
+
+if __name__=='__main__':
+    distribution_plots(data, output_directory)
---
+++ Linear_Regression/preprocessing.py
@@ -0,0 +1,39 @@
+#outlier handling
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+import os
+
+def load_data(file_path):
+    return pd.read_csv(file_path)
+
+def handle_outliers(df, column):
+    Q1=df[column].quantile(0.25)
+    Q3=df[column].quantile(0.75)
+    IQR=Q3-Q1
+    lower_limit=Q1-1.5*IQR
+    upper_limit=Q3+1.5*IQR
+    
+    median=df[column].median()
+    df[column] = np.where(np.logical_or(df[column] > upper_limit, df[column] < lower_limit), median, df[column])
+    return df
+def save_data(df, file_path):
+    df.to_csv(file_path, index=False)
+    
+def main():
+    cleaned_data= 'cleaned_data.csv'
+    df=load_data('Advertising.csv')
+    numeric_columns=df.select_dtypes(include='number').columns
+
+    for column in numeric_columns:
+        df=handle_outliers(df,column)
+    save_data(df, cleaned_data)
+    print('Data processing cpmplete. Cleaned data saved to')
+    
+if __name__ == "__main__":
+    main()
+    
+    
+    
+    
